f90de56ec3a0f62f0d3ac7c1aa471a4f155e7415
Ishika Gupta <ishikagupta@Ishikas-MacBook-Air.local>
2025-07-09 01:51:29 +0530

    first commit

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -95,51 +95,51 @@ Your favorite books (in no particular order)
             </tr>
                         <tr>
               <td>Q4</td>
-              <td>CSS Flexbox</td>
-              <td>2/24/21</td>
-              <td>92</td>
+              <td>CSS</td>
+              <td>2/30/21</td>
+              <td>93</td>
             </tr>
             <tr>
               <td>Q5</td>
-              <td>React JSX</td>
+              <td>React</td>
               <td>3/3/21</td>
-              <td>89</td>
+              <td>92</td>
             </tr>
             <tr>
               <td>Q6</td>
               <td>TypeScript</td>
-              <td>3/10/21</td>
-              <td>94</td>
+              <td>3/15/21</td>
+              <td>95</td>
             </tr>
             <tr>
               <td>Q7</td>
-              <td>Node.js</td>
-              <td>3/17/21</td>
-              <td>90</td>
+              <td>Node</td>
+              <td>3/22/21</td>
+              <td>92</td>
             </tr>
             <tr>
               <td>Q8</td>
-              <td>Express Routing</td>
-              <td>3/24/21</td>
-              <td>93</td>
+              <td>Routing</td>
+              <td>3/29/21</td>
+              <td>97</td>
             </tr>
             <tr>
               <td>Q9</td>
               <td>MongoDB</td>
-              <td>3/31/21</td>
+              <td>4/03/21</td>
               <td>95</td>
             </tr>
             <tr>
               <td>Q10</td>
               <td>Final Project</td>
-              <td>4/7/21</td>
+              <td>4/18/21</td>
               <td>97</td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
               <td colSpan={3}>Average</td>
-              <td>91.3</td>
+              <td>92.4</td>
             </tr>
           </tfoot>
         </table>
